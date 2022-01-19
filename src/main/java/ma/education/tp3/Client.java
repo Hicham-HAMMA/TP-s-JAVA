62cf0191be3391a6414ffc03f687146c43ae9de6
Hicham-HAMMA <hishemh@gmail.com>
2022-01-19 01:02:49 +0100

    TP3

---
+++ src/main/java/ma/education/tp3/Client.java
@@ -0,0 +1,12 @@
+package ma.education.tp3;
+
+public class Client {
+        private long id;
+        private String name;
+        private Client() {
+            System.out.println("Acces Constructor Client..");
+
+        }
+
+
+}

63f1c076ddea500340052459e21c17e29f54992c
Hicham-HAMMA <hishemh@gmail.com>
2022-01-19 01:52:45 +0100

    TP4

---
+++ src/ma/education/tp/annotations/Calculatrice.java
@@ -0,0 +1,8 @@
+package ma.education.tp.annotations;
+
+
+import java.lang.annotation.Inherited;
+
+@Programmer(id=10,name="Said ALAMI")
+public class Calculatrice {
+}
---
+++ src/ma/education/tp/annotations/Programmer.java
@@ -0,0 +1,11 @@
+package ma.education.tp.annotations;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Programmer {
+    abstract int id();
+    String name();
+}
